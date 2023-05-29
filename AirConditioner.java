db27eb48b641a0cac34d0ea947b815cfb4c36dfe
Ashley <ashleyndabai@gmail.com>
2023-05-29 07:01:03 +0100

    Add files via upload

---
+++ AirConditioner.java
@@ -0,0 +1,34 @@
+public class AirConditioner {
+    private boolean isOn;
+    private String productName;
+    private int temperature;
+
+    public String AirConditioner() {
+        }
+    public void decreaseTemperature() {
+            return temperature;
+        } 
+    public String getProductName() {
+            return ProductName;
+        }
+    public int getTemperature() {
+            return Temperature;
+        }
+    public void increaseTemperature() {
+        }
+    public boolean isOn (boolean isOn) {
+            return ison;
+        }
+    public boolean setOn(boolean on) {
+           if (IsOn == true){
+           SetOn = on;
+         }  
+        }
+    public String setProductName(void) {
+            this.productName = productName;
+        }
+    public int setTemperature() {
+            this.temperature = temperature;
+        }
+    }
+
